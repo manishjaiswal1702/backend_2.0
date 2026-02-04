4e7a379fd66c7f95c4e8af790448cf6a41ed5b7c
manishjaiswal1702 <manishjaiswal4043@gmail.com>
2026-02-04 22:14:22 +0530

    deployment starting

--- day-9/Backend/src/app.js
+++ day-9/Backend/src/app.js
@@ -5,10 +5,12 @@
 const express = require("express")
 const noteModel = require("./models/note.model")
 const cors = require("cors")
+const path = require("path")
 
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(express.static("./public"))
 
 /**
  * - POST /api/notes
