e511c8a103e486e4a90994bd8aa164adb10926ba
manishjaiswal1702 <manishjaiswal4043@gmail.com>
2026-02-13 18:03:54 +0530

    post created

--- Project_1/Backend/src/controllers/post.controller.js
+++ Project_1/Backend/src/controllers/post.controller.js
@@ -1,7 +1,7 @@
 const postModel = require("../models/post.model")
 const ImageKit = require("@imagekit/nodejs")
 const { toFile } = require("@imagekit/nodejs")
-
+const jwt = require("jsonwebtoken")
 
 const imagekit = new ImageKit({
     privateKey: process.env.IMAGEKIT_PRIVATE_KEY
@@ -10,12 +10,40 @@ const imagekit = new ImageKit({
 async function createPostController(req, res) {
     console.log(req.body, req.file)
 
+    const token = req.cookies.token
+
+    if (!token) {
+        return res.status(401).json({
+            message: "token not provided,unauthorized access"
+        })
+    }
+
+    let decoded;
+
+    try {
+        decoded = jwt.verify(token, process.env.JWT_SECRET)
+    } catch (err) {
+        return res.status(401).json({
+            message: "user not authaorized"
+        })
+    }
+
     const file = await imagekit.files.upload({
         file: await toFile(Buffer.from(req.file.buffer), 'file'),
-        fileName: "Test"
+        fileName: "Test",
+        folder: "cohort-2-0-intsa-posts"
     })
 
-    res.send(file)
+    const post = await postModel.create({
+        caption: req.body.caption,
+        imgUrl: file.url,
+        user: decoded.id
+    })
+
+    res.status(201).json({
+        message: "Post created successfully",
+        post
+    })
 
 }
 
