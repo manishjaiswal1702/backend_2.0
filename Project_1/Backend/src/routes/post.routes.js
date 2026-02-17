06ae51912e4b21330c86e966aa4a4c0c4a9e7ca7
manishjaiswal1702 <manishjaiswal4043@gmail.com>
2026-02-17 18:42:08 +0530

    middleware

--- Project_1/Backend/src/routes/post.routes.js
+++ Project_1/Backend/src/routes/post.routes.js
@@ -3,26 +3,27 @@ const postRouter = express.Router()
 const postController = require("../controllers/post.controller")
 const multer = require("multer")
 const upload = multer({ storage: multer.memoryStorage() })
+const identifyUser = require("../middlewares/auth.middleware")
 
 
 /**
  * POST /api/posts [protected]
  * - req.body = { caption,image-file }
  */
-postRouter.post("/", upload.single("chacha"), postController.createPostController)
+postRouter.post("/", upload.single("chacha"), identifyUser, postController.createPostController)
 
 
 /**
  * GET /api/posts/ [protected]
  */
-postRouter.get("/", postController.getPostController)
+postRouter.get("/", identifyUser, postController.getPostController)
 
 
 /**
  * GET /api/posts/details/:postid
  * - return an detail about specific post with the id. also check whether the post belongs to the user that the request come from
  */
-postRouter.get("/details/:postId", postController.getPostDetailsController)
+postRouter.get("/details/:postId", identifyUser, postController.getPostDetailsController)
 
 
 module.exports = postRouter
