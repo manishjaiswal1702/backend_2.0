6421f9a67a796fcfb1d70eb5193a96efb7d7902b
manishjaiswal1702 <manishjaiswal4043@gmail.com>
2026-02-18 09:57:32 +0530

    day-18

--- Project_1/Backend/server.js
+++ Project_1/Backend/server.js
@@ -4,6 +4,7 @@ const connectToDatabase = require("./src/config/database")
 
 connectToDatabase()
 
+/* start the server */
 app.listen(3000, () => {
     console.log("Server is running on port 3000")
 })
