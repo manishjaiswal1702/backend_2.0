c81187f85e374d9fbe4985747c23f0a2cbe3693e
manishjaiswal1702 <manishjaiswal4043@gmail.com>
2026-02-20 10:14:46 +0530

    frontend commit

--- Project_1/Frontend/src/App.jsx
+++ Project_1/Frontend/src/App.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import { RouterProvider } from "react-router"
+import AppRoutes from "./AppRoutes"
+import "./style.scss"
+import { AuthProvider } from "./features/auth/auth.context.jsx"
 
-const App = () => {
+function App() {
   return (
-    <div>
-      
-    </div>
+    <AuthProvider>
+      <AppRoutes />
+    </AuthProvider>
   )
 }
 
