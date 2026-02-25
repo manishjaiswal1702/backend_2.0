8c13655f3eb23455696160cd8a79340654ffe268
manishjaiswal1702 <manishjaiswal4043@gmail.com>
2026-02-26 00:35:54 +0530

    frontend only auth

--- Project_1/Frontend/src/App.jsx
+++ Project_1/Frontend/src/App.jsx
@@ -2,13 +2,16 @@ import { RouterProvider } from "react-router"
 import { router } from "./app.routes"
 import { AuthProvider } from "./features/auth/auth.context"
 import "./features/shared/global.scss"
+import { PostContextProvider } from "./features/posts/post.context"
 
 
 function App() {
 
   return (
     <AuthProvider>
-      <RouterProvider router={router} />
+      <PostContextProvider>
+        <RouterProvider router={router} />
+      </PostContextProvider>
     </AuthProvider>
   )
 }
--- Project_1/Frontend/src/features/auth/auth.Context.jsx
+++ Project_1/Frontend/src/features/auth/auth.Context.jsx
@@ -6,10 +6,10 @@ export const AuthContext = createContext()
 
 export const AuthProvider = ({ children }) => {
 
-    const [ user, setUser ] = useState(null)
-    const [ loading, setLoading ] = useState(false)
+    const [user, setUser] = useState(null)
+    const [loading, setLoading] = useState(false)
 
-    
+
     return (
         <AuthContext.Provider value={{ user, setUser, loading, setLoading }} >
             {children}
