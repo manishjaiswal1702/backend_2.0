2b1abc190c939724a84133ed8863a857b0d4dde9
manishjaiswal1702 <manishjaiswal4043@gmail.com>
2026-03-02 23:36:36 +0530

    REDIS

--- Moodify/Backend/src/controllers/auth.controller.js
+++ Moodify/Backend/src/controllers/auth.controller.js
@@ -2,6 +2,7 @@ const userModel = require("../models/user.model");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const blacklistModel = require("../models/blacklist.model")
+const redis = require("../config/cache")
 
 async function registerUser(req, res) {
     const { username, email, password } = req.body;
@@ -114,15 +115,23 @@ async function logoutUser(req, res) {
 
     res.clearCookie("token")
 
-    await blacklistModel.create({
-        token
-    })
+    await redis.set(token, Date.now().toString(), "EX", 60 * 60)
 
     res.status(200).json({
         message: "logout successfully."
     })
 
 }
+
+
+/**
+ * key value
+ * js object
+ * {
+ *  username:'test',
+ *  email:'test@test.com'
+ * }
+ */
 
 
 module.exports = { registerUser, loginUser, getMe, logoutUser }
--- Moodify/Backend/src/middlewares/auth.middleware.js
+++ Moodify/Backend/src/middlewares/auth.middleware.js
@@ -1,5 +1,6 @@
 const blacklistModel = require("../models/blacklist.model");
 const userModel = require("../models/user.model");
+const redis = require("../config/cache")
 const jwt = require("jsonwebtoken");
 
 
@@ -12,9 +13,7 @@ async function authUser(req, res, next) {
         })
     }
 
-    const isTokenBlacklisted = await blacklistModel.findOne({
-        token
-    })
+    const isTokenBlacklisted = await redis.get(token)
 
     if (isTokenBlacklisted) {
         return res.status(401).json({
