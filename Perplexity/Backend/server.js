1d8a5648c8d39255443c800469d3cf19de2b3d2f
manishjaiswal1702 <manishjaiswal4043@gmail.com>
2026-03-16 11:14:22 +0530

    email send intigration

--- Perplexity/Backend/server.js
+++ Perplexity/Backend/server.js
@@ -1,15 +1,8 @@
 import "dotenv/config";
 import app from "./src/app.js";
-import http from "http";
 import connectDB from "./src/config/database.js";
-import { initSocket } from "./src/sockets/server.socket.js";
 
 const PORT = process.env.PORT || 8000;
-
-
-const httpServer = http.createServer(app);
-
-initSocket(httpServer);
 
 connectDB()
     .catch((err) => {
@@ -17,6 +10,6 @@ connectDB()
         process.exit(1);
     });
 
-httpServer.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
