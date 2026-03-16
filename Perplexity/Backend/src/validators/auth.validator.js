6f6d3c32bf72b8861a65564e690105f0bf65ab56
manishjaiswal1702 <manishjaiswal4043@gmail.com>
2026-03-17 00:24:25 +0530

    ...

--- Perplexity/Backend/src/validators/auth.validator.js
+++ Perplexity/Backend/src/validators/auth.validator.js
@@ -28,3 +28,15 @@ export const registerValidator = [
     validate
 ];
 
+export const loginValidator = [
+    body("email")
+        .trim()
+        .notEmpty().withMessage("Email is required")
+        .isEmail().withMessage("Please provide a valid email"),
+
+    body("password")
+        .notEmpty().withMessage("Password is required"),
+
+    validate
+];
+
