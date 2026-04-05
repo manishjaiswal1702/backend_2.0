38a43de673f34065441e7a1d6a956a672daa84ce
manishjaiswal1702 <manishjaiswal4043@gmail.com>
2026-04-05 19:52:12 +0530

    mpv done

--- Ai-Battle/src/app.ts
+++ Ai-Battle/src/app.ts
@@ -1,10 +1,16 @@
-import express from 'express'
+import express from 'express';
+import runGraph from "./ai/graph.ai.js";
 
-const app = express()
+const app = express();
 
 
-app.get('/health', (req, res) => {
-    res.status(200).json({ status: 'ok' })
+app.get('/', async (req: any, res: any) => {
+
+    const result = await runGraph("Write an code for Factorial function in js")
+
+    res.json(result)
 })
 
-export default app
+
+
+export default app;
