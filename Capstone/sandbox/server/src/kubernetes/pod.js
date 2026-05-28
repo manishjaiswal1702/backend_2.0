045b9d07f73c0a6fca4da5e033bdcf4b0d668596
manishjaiswal1702 <manishjaiswal4043@gmail.com>
2026-05-29 00:09:36 +0530

    day 3 - capstone project

--- Capstone/sandbox/server/src/kubernetes/pod.js
+++ Capstone/sandbox/server/src/kubernetes/pod.js
@@ -12,16 +12,58 @@ export async function createPod(sandboxId) {
             }
         },
         spec: {
+            volumes: [
+                {
+                    name: 'workspace-volume',
+                    emptyDir: {}
+                }
+            ],
+            initContainers: [
+                {
+                    name: 'init-container',
+                    image: "template",
+                    imagePullPolicy: "IfNotPresent",
+                    command: ['sh', '-c', 'cp -r /workspace/. /seed/'],
+                    volumeMounts: [
+                        {
+                            name: 'workspace-volume',
+                            mountPath: '/seed'
+                        }
+                    ]
+                }
+            ],
             containers: [
                 {
                     image: "template",
                     imagePullPolicy: "IfNotPresent",
                     name: 'sandbox-container',
-                    ports: [ { containerPort: 5173, name: "http" } ],
+                    ports: [{ containerPort: 5173, name: "http" }],
                     resources: {
                         limits: { cpu: "500m", memory: "1Gi" },
                         requests: { cpu: "250m", memory: "500Mi" }
-                    }
+                    },
+                    volumeMounts: [
+                        {
+                            name: 'workspace-volume',
+                            mountPath: '/workspace'
+                        }
+                    ]
+                },
+                {
+                    image: "agent",
+                    imagePullPolicy: "IfNotPresent",
+                    name: 'agent-container',
+                    ports: [{ containerPort: 3000, name: "http" }],
+                    resources: {
+                        limits: { cpu: "500m", memory: "1Gi" },
+                        requests: { cpu: "250m", memory: "500Mi" }
+                    },
+                    volumeMounts: [
+                        {
+                            name: 'workspace-volume',
+                            mountPath: '/workspace'
+                        }
+                    ]
                 }
             ]
         }
